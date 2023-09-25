2be53ef3866b16478825fdfbf95427a50f1ee8d1
jenstem <124745888+jenstem@users.noreply.github.com>
2023-09-25 16:04:10 -0400

    Add blogData.js to seeds folder

--- seeds/blogData.js
+++ seeds/blogData.js
@@ -1,1 +1,37 @@
+const { Blog } = require('../models');
 
+const blogData = [
+    {
+        "title": "Music Near Me",
+        "user_id": 1,
+        "post": "A mobile app that will send you notifications whenever a concert is playing in your area.",
+    },
+
+    {
+        "title": "The Ultimate Tech Quiz",
+        "user_id": 2,
+        "post": "A web app that will give users 10 new technical questions each day and track their progress in things like programming, cybersecurity, database architecture, and more!",
+    },
+
+    {
+        "title": "Roll 'Em Up",
+        "user_id": 3,
+        "post": "A game for Windows and macOS where players move a ball through a series of increasingly challenging mazes.",
+    },
+
+    {
+        "title": "The Ultimate Tech Quiz 2",
+        "user_id": 2,
+        "post": "A web app that will give users 10 new technical questions each day and track their progress in things like programming, cybersecurity, database architecture, and more!",
+    },
+
+    {
+        "title": "Roll 'Em Up 2",
+        "user_id": 3,
+        "post": "A game for Windows and macOS where players move a ball through a series of increasingly challenging mazes.",
+    }
+];
+
+const seedAllPosts = () => Blog.bulkCreate(blogData);
+
+module.exports = seedAllPosts;
