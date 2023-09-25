f8962bb9bb536f2f66b770b10ab053688c61cb9c
jenstem <124745888+jenstem@users.noreply.github.com>
2023-09-25 16:10:56 -0400

    Add index.js to models folder

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Blog = require('./Blog');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog, Comment };
