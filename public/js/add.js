973768cba8229ea9b2c7eca770ece90ba794410e
jenstem <124745888+jenstem@users.noreply.github.com>
2023-09-25 16:07:08 -0400

    Add add.js to js folder

--- public/js/add.js
+++ public/js/add.js
@@ -1,1 +1,22 @@
+const addFormHandler = async (event) => {
+  event.preventDefault();
 
+  const title = document.querySelector('#blog-title').value.trim();
+  const post = document.querySelector('#blog-post').value.trim();
+
+  if (title && post) {
+    const response = await fetch('/api/Blogs', {
+      method: 'POST',
+      body: JSON.stringify({ title, post }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.getElementById('add-blog-post').addEventListener('submit', addFormHandler);
