8fed16f45876af7c1fa6a501661dac21813370a6
jenstem <124745888+jenstem@users.noreply.github.com>
2023-09-26 17:37:47 -0400

    Revise server.js order

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
+const expressHandlebars = require('express-handlebars');
+const path = require('path');
+require("body-parser");
+const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const routes = require('./controllers');
-
-const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const sess = {
   secret: 'Super secret secret',
@@ -26,13 +28,19 @@ const sess = {
 
 app.use(session(sess));
 
+const exphbs = expressHandlebars.create({
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  extname: '.handlebars',
+  helpers: helpers
+});
+
+const hbs = expressHandlebars.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+const routes = require('./controllers/index.js');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
